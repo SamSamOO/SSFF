03e27d09839102a8eecc9b6cb76c68c7edca2b79
sn022 <sn022317@gmail.com>
2021-11-01 19:23:50 +0900

    로그 순서 변경

--- src/main/java/kr/or/ssff/controller/HomeController.java
+++ src/main/java/kr/or/ssff/controller/HomeController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Handles requests for the application home page.
  * 테스트!
  */
+
 @Controller
 public class HomeController {
 	
@@ -25,6 +26,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
+
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
