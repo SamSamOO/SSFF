27b8f6b5815134334fb6c7ef59df70cc1f59a578
sn022 <sn022317@gmail.com>
2021-11-06 21:01:38 +0900

    1. servlet context.xml에 member 구문 추가 2. member apply 관련 파일 생성

--- src/main/java/kr/or/ssff/member/service/MemberService.java
+++ src/main/java/kr/or/ssff/member/service/MemberService.java
@@ -1,35 +1,17 @@
 package kr.or.ssff.member.service;
 
+import java.util.List;
+
+import kr.or.ssff.member.model.ApplyMemberVO;
+
 /*
-
+ * 최초 작성자: 신지혜
+ * 목적: member service 관리
  */
-public class MemberService {
 
-    public void memberJoin() {
-    }
-}
+public interface MemberService {
 
+	// 1. 특정 스터디의 멤버 조회 : 신지혜 
+	public abstract List<ApplyMemberVO> getApplyMemberList(Integer r_idx);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+} // end interface
